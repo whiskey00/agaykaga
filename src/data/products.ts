1e37bcd065a4d1d66558cb3244d204148aca9f67
agaykaga <119928947+whiskey00@users.noreply.github.com>
2025-08-29 14:22:54 +0800

    test

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,5 +1,5 @@
 // Import sample flower images
-import flower1 from '../images/sample_flowers/1.png';
+import flower1 from '../images/sample_flowers/1.jpg';
 import flower2 from '../images/sample_flowers/2.png';
 import flower3 from '../images/sample_flowers/3.png';
 import flower4 from '../images/sample_flowers/4.png';
